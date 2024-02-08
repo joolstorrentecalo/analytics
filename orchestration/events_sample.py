4a570a3205e2afc535aefd6e3f27f03e6e0a27ec
Radovan Bacovic <rbacovic@gitlab.com>
2024-02-08 10:07:09 +0100

    Add formatting.

--- orchestration/events_sample.py
+++ orchestration/events_sample.py
@@ -23,7 +23,7 @@ def main(engine: Engine) -> None:
     WITH raw AS (
     SELECT * EXCLUDE (geo_zipcode, geo_latitude, geo_longitude, user_ipaddress)
     FROM raw.snowplow.gitlab_events
-    WHERE uploaded_at::DATE > dateadd(day, -7, current_date)::DATE
+    WHERE uploaded_at::DATE > dateadd(day, -1, current_date)::DATE
     LIMIT 5000000)
     SELECT *,
     NULL AS geo_zipcode,
