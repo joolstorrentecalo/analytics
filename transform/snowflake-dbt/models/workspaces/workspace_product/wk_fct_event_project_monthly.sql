ae895f221e3742821b0cb6ad7bde1f0dcd9d2f29
Dave Peterson <dpeterson@gitlab.com>
2024-05-13 20:56:16 +0000

    edit for crm account

--- transform/snowflake-dbt/models/workspaces/workspace_product/wk_fct_event_project_monthly.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/wk_fct_event_project_monthly.sql
@@ -6,7 +6,8 @@
 {{ simple_cte([
     ('fct_event_valid', 'fct_event_valid'),
     ('dim_date', 'dim_date'),
-    ('dim_project','dim_project')
+    ('dim_project','dim_project'),
+    ('dim_crm_accounts', 'dim_crm_account')
     ])
 }},
 
@@ -71,6 +72,7 @@ fct_event_project_monthly AS (
       fact_with_month.dim_latest_product_tier_id,
       fact_with_month.dim_latest_subscription_id,
       fact_with_month.dim_crm_account_id,
+      dim_crm_account.crm_account_name,
       fact_with_month.dim_billing_account_id,
       project.project_created_month,
       project.project_creator_id,
@@ -100,9 +102,11 @@ fct_event_project_monthly AS (
     INNER JOIN project
       ON fact_with_month.dim_project_id = project.dim_project_id
       AND fact_with_month.dim_ultimate_parent_namespace_id = project.ultimate_parent_namespace_id
+    LEFT JOIN dim_crm_account --To include the crm_account_name field
+      ON dim_crm_account.dim_crm_account_id = fact_with_month.dim_crm_account_id
     WHERE fact_with_month.dim_ultimate_parent_namespace_id IS NOT NULL
       AND fact_with_month.dim_project_id IS NOT NULL
-    {{ dbt_utils.group_by(n=21) }}
+    {{ dbt_utils.group_by(n=22) }}
         
 )
 
@@ -110,6 +114,6 @@ fct_event_project_monthly AS (
     cte_ref="fct_event_project_monthly",
     created_by="@dpeterson",
     updated_by="@dpeterson",
-    created_date="2024-05-10",
-    updated_date="2024-05-10"
+    created_date="2024-05-13",
+    updated_date="2024-05-13"
 ) }}
