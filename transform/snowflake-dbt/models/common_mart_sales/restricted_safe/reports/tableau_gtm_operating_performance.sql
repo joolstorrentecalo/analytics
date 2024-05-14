cfa68cc65f0b7c54551d604879527ccd9f5d5b31
Chris Sharp <csharp@gitlab.com>
2024-05-14 16:50:34 +0100

    add fields

--- transform/snowflake-dbt/models/common_mart_sales/restricted_safe/reports/tableau_gtm_operating_performance.sql
+++ transform/snowflake-dbt/models/common_mart_sales/restricted_safe/reports/tableau_gtm_operating_performance.sql
@@ -6,7 +6,8 @@
     ('dim_sales_funnel_kpi', 'dim_sales_funnel_kpi'),
     ('dim_crm_user_hierarchy', 'dim_crm_user_hierarchy'),
     ('dim_order_type', 'dim_order_type'),
-    ('dim_sales_qualified_source', 'dim_sales_qualified_source')
+    ('dim_sales_qualified_source', 'dim_sales_qualified_source'),
+    ('mart_crm_opportunity', 'mart_crm_opportunity')
 ]) }}
 
 , scaffold AS (
@@ -19,7 +20,9 @@
       rpt_scaffold.dim_order_type_id,
       rpt_scaffold.dim_sales_qualified_source_id,
       fct_sales_funnel_actual.dim_crm_opportunity_id,
+      fct_sales_funnel_actual.email_hash,
       fct_sales_funnel_actual.net_arr,
+      fct_sales_funnel_actual.new_logo_count,
       fct_sales_funnel_target_daily.daily_allocated_target
     FROM rpt_scaffold
     INNER JOIN dim_date on rpt_scaffold.date_id = dim_date.date_id
@@ -53,10 +56,24 @@
       dim_crm_user_hierarchy.crm_user_business_unit,
       dim_crm_user_hierarchy.crm_user_geo,
       dim_crm_user_hierarchy.crm_user_region,
+      dim_crm_user_hierarchy.crm_user_sales_segment,
       dim_order_type.order_type_name,
       dim_sales_qualified_source.sales_qualified_source_name,
+      CASE WHEN mart_crm_opportunity.product_category = 'Dedicated - Ultimate' THEN 'Dedicated - Ultimate'
+        WHEN mart_crm_opportunity.product_category IN ('Self-Managed - Premium', 'SaaS - Premium','Premium - 1 Year','Premium') THEN 'Premium'
+        WHEN mart_crm_opportunity.product_category IN ('Ultimate' , 'Self-Managed - Ultimate' , 'SaaS - Ultimate') THEN 'Ultimate'
+        ELSE 'Other' 
+      END                                                           AS product_category_modified,
+      CASE WHEN scaffold.new_logo_count >= 0
+        THEN scaffold.dim_crm_opportunity_id 
+      END                                                           AS first_number_deals,
+      CASE WHEN scaffold.new_logo_count = -1
+        THEN scaffold.dim_crm_opportunity_id 
+      END                                                           AS second_number_deals,
       scaffold.net_arr,
+      scaffold.new_logo_count,
       scaffold.daily_allocated_target,
+      scaffold.email_hash,
       scaffold.dim_crm_opportunity_id
 FROM scaffold
 INNER JOIN dim_sales_funnel_kpi
@@ -66,7 +83,9 @@ INNER JOIN dim_crm_user_hierarchy
 INNER JOIN dim_order_type
   ON scaffold.dim_order_type_id = dim_order_type.dim_order_type_id
 INNER JOIN dim_sales_qualified_source
- ON scaffold.dim_sales_qualified_source_id = dim_sales_qualified_source.dim_sales_qualified_source_id
+  ON scaffold.dim_sales_qualified_source_id = dim_sales_qualified_source.dim_sales_qualified_source_id
+INNER JOIN mart_crm_opportunity
+  ON scaffold.dim_crm_opportunity_id = mart_crm_opportunity.dim_crm_opportunity_id
 )
 
 SELECT 
