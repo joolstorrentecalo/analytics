1375ea026820935565432238064785df407a9ef7
Utkarsh <usinghal@gitlab.com>
2024-07-10 21:11:42 +0530

    remove equal to from utm param & audit macro

--- transform/snowflake-dbt/models/common/facts_sales_and_marketing/fct_behavior_website_session.sql
+++ transform/snowflake-dbt/models/common/facts_sales_and_marketing/fct_behavior_website_session.sql
@@ -9,119 +9,109 @@
 {{ simple_cte([
     ('sessions', 'prep_snowplow_sessions_all')
     ])
-}},
+}}
 
-sessions_w_dim AS (
-  SELECT
-    -- Primary Key
-    {{ dbt_utils.generate_surrogate_key(['session_id','session_end']) }}                           AS fct_behavior_website_session_pk,
 
-    -- Natural Keys
-    app_id,
-    session_id,
+SELECT
+  -- Primary Key
+  {{ dbt_utils.generate_surrogate_key(['session_id','session_end']) }}                           AS fct_behavior_website_session_pk,
 
-    -- Foreign Keys
-    {{ dbt_utils.generate_surrogate_key([
-      'browser_name',
-      'browser_major_version',
-      'browser_minor_version',
-      'browser_language'])
-    }}                                                                                             AS dim_behavior_browser_sk,
+  -- Natural Keys
+  app_id,
+  session_id,
 
-    {{ dbt_utils.generate_surrogate_key([
-      'os_name',
-      'os_timezone'
-    ]) }}                                                                                          AS dim_behavior_operating_system_sk,
+  -- Foreign Keys
+  {{ dbt_utils.generate_surrogate_key([
+    'browser_name',
+    'browser_major_version',
+    'browser_minor_version',
+    'browser_language'])
+  }}                                                                                             AS dim_behavior_browser_sk,
 
-    {{ dbt_utils.generate_surrogate_key(['first_page_url', 'app_id', 'first_page_url_scheme']) }}  AS dim_behavior_website_page_sk,
-    
-    {{ dbt_utils.generate_surrogate_key(['referer_url', 'app_id', 'referer_url_scheme']) }}        AS dim_behavior_referrer_page_sk,
+  {{ dbt_utils.generate_surrogate_key([
+    'os_name',
+    'os_timezone'
+  ]) }}                                                                                          AS dim_behavior_operating_system_sk,
 
-    --Time Attributes
-    session_start,
-    session_end,
-    session_start                                                                                  AS behavior_at,
-    session_start_local,
-    session_end_local,
+  {{ dbt_utils.generate_surrogate_key(['first_page_url', 'app_id', 'first_page_url_scheme']) }}  AS dim_behavior_website_page_sk,
+  
+  {{ dbt_utils.generate_surrogate_key(['referer_url', 'app_id', 'referer_url_scheme']) }}        AS dim_behavior_referrer_page_sk,
 
-    -- User Attributes
-    inferred_user_id,
-    user_snowplow_domain_id,
-    user_snowplow_crossdomain_id,
+  --Time Attributes
+  session_start,
+  session_end,
+  session_start                                                                                  AS behavior_at,
+  session_start_local,
+  session_end_local,
 
-    -- First Gitlab Standard Context Attributes
-    first_gsc_pseudonymized_user_id,
-    first_gsc_project_id,
-    first_gsc_namespace_id,
-    first_gsc_google_analytics_client_id,
-    first_gsc_environment,
-    first_gsc_is_gitlab_team_member,
-    first_gsc_plan,
-    first_gsc_source,
+  -- User Attributes
+  inferred_user_id,
+  user_snowplow_domain_id,
+  user_snowplow_crossdomain_id,
 
-    -- Last Gitlab Standard Context Attributes
-    last_gsc_pseudonymized_user_id,
-    last_gsc_project_id,
-    last_gsc_namespace_id,
-    last_gsc_google_analytics_client_id,
-    last_gsc_environment,
-    last_gsc_is_gitlab_team_member,
-    last_gsc_plan,
-    last_gsc_source,
+  -- First Gitlab Standard Context Attributes
+  first_gsc_pseudonymized_user_id,
+  first_gsc_project_id,
+  first_gsc_namespace_id,
+  first_gsc_google_analytics_client_id,
+  first_gsc_environment,
+  first_gsc_is_gitlab_team_member,
+  first_gsc_plan,
+  first_gsc_source,
 
-    -- User Location Attributes
-    COALESCE(geo_city, 'Unknown')::VARCHAR                                                           AS user_city,
-    COALESCE(geo_country, 'Unknown')::VARCHAR                                                        AS user_country,
-    COALESCE(geo_region, 'Unknown')::VARCHAR                                                         AS user_region,
-    COALESCE(geo_region_name, 'Unknown')::VARCHAR                                                    AS user_region_name,
-    COALESCE(geo_timezone, 'Unknown')::VARCHAR                                                       AS user_timezone_name,
+  -- Last Gitlab Standard Context Attributes
+  last_gsc_pseudonymized_user_id,
+  last_gsc_project_id,
+  last_gsc_namespace_id,
+  last_gsc_google_analytics_client_id,
+  last_gsc_environment,
+  last_gsc_is_gitlab_team_member,
+  last_gsc_plan,
+  last_gsc_source,
 
-    -- First page Attributes
-    first_page_title,
-    first_page_url,
-    first_page_url_fragment,
-    first_page_url_host,
-    first_page_url_path,
-    first_page_url_port,
-    first_page_url_query,
-    first_page_url_scheme,
+  -- User Location Attributes
+  COALESCE(geo_city, 'Unknown')::VARCHAR                                                           AS user_city,
+  COALESCE(geo_country, 'Unknown')::VARCHAR                                                        AS user_country,
+  COALESCE(geo_region, 'Unknown')::VARCHAR                                                         AS user_region,
+  COALESCE(geo_region_name, 'Unknown')::VARCHAR                                                    AS user_region_name,
+  COALESCE(geo_timezone, 'Unknown')::VARCHAR                                                       AS user_timezone_name,
 
-    exit_page_url,
+  -- First page Attributes
+  first_page_title,
+  first_page_url,
+  first_page_url_fragment,
+  first_page_url_host,
+  first_page_url_path,
+  first_page_url_port,
+  first_page_url_query,
+  first_page_url_scheme,
 
-    -- UTM Attributes
-    {{ dbt_utils.get_url_parameter(field='first_page_url_query', url_parameter='glm_source=') }}   AS glm_source,
-    {{ dbt_utils.get_url_parameter(field='first_page_url_query', url_parameter='utm_campaign=') }} AS utm_campaign,
-    {{ dbt_utils.get_url_parameter(field='first_page_url_query', url_parameter='utm_content=') }}  AS utm_content,
-    {{ dbt_utils.get_url_parameter(field='first_page_url_query', url_parameter='utm_medium=') }}   AS utm_medium,
-    {{ dbt_utils.get_url_parameter(field='first_page_url_query', url_parameter='utm_source=') }}   AS utm_source,
-    {{ dbt_utils.get_url_parameter(field='first_page_url_query', url_parameter='utm_term=') }}     AS utm_term,
+  exit_page_url,
 
-    -- Referer Attributes
-    referer_url,
-    referer_url_fragment,
-    referer_url_port,
+  -- UTM Attributes
+  {{ dbt_utils.get_url_parameter(field='first_page_url_query', url_parameter='glm_source') }}   AS glm_source,
+  {{ dbt_utils.get_url_parameter(field='first_page_url_query', url_parameter='utm_campaign') }} AS utm_campaign,
+  {{ dbt_utils.get_url_parameter(field='first_page_url_query', url_parameter='utm_content') }}  AS utm_content,
+  {{ dbt_utils.get_url_parameter(field='first_page_url_query', url_parameter='utm_medium') }}   AS utm_medium,
+  {{ dbt_utils.get_url_parameter(field='first_page_url_query', url_parameter='utm_source') }}   AS utm_source,
+  {{ dbt_utils.get_url_parameter(field='first_page_url_query', url_parameter='utm_term') }}     AS utm_term,
 
-    -- Session Attributes
-    session_index,
-    session_cookie_index,
-    page_views,
-    user_bounced                                                                                   AS is_user_bounced,
-    time_engaged_in_s                                                                              AS engaged_seconds,
-    time_engaged_in_s_tier                                                                         AS engaged_seconds_range
-  FROM sessions
+  -- Referer Attributes
+  referer_url,
+  referer_url_fragment,
+  referer_url_port,
 
-  {% if is_incremental() %}
+  -- Session Attributes
+  session_index,
+  session_cookie_index,
+  page_views,
+  user_bounced                                                                                   AS is_user_bounced,
+  time_engaged_in_s                                                                              AS engaged_seconds,
+  time_engaged_in_s_tier                                                                         AS engaged_seconds_range
+FROM sessions
 
-  WHERE session_end > (SELECT max(session_end) FROM {{ this }})
+{% if is_incremental() %}
 
-  {% endif %}
+WHERE session_end > (SELECT max(session_end) FROM {{ this }})
 
-)
-
-{{ dbt_audit(
-    cte_ref="sessions_w_dim",
-    created_by="@utkarsh060",
-    updated_by="@utkarsh060",
-    created_date="2024-07-02",
-    updated_date="2024-07-02"
-) }}
+{% endif %}
