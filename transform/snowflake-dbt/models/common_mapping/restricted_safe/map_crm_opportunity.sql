7135ed42bbda1aa27e9f9d8e8e047aa68e2450c8
Chris Sharp <csharp@gitlab.com>
2024-07-18 12:02:46 +0100

    replicate filters on sfdc_opportunity

--- transform/snowflake-dbt/models/common_mapping/restricted_safe/map_crm_opportunity.sql
+++ transform/snowflake-dbt/models/common_mapping/restricted_safe/map_crm_opportunity.sql
@@ -27,6 +27,8 @@ WITH crm_account_dimensions AS (
 
     SELECT *
     FROM {{ ref('sfdc_opportunity_source') }}
+    WHERE account_id IS NOT NULL
+    AND is_deleted = FALSE
 
 ), opportunity_fields AS(
 
