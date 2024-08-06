d99199de16b26d0723356076c812e5c94f672600
Peter Empey <pempey@gitlab.com>
2024-08-06 09:20:32 -0600

    v2

--- transform/snowflake-dbt/dbt_model_performance_check.py
+++ transform/snowflake-dbt/dbt_model_performance_check.py
@@ -3,133 +3,11 @@ Module returning a csv with downstream dbt dependencies and
 data science exposures for a list of model provided by the user.
 """
 
-import os
-import yaml
 import json
 
 import pandas as pd
-from dbt.cli.main import dbtRunner, dbtRunnerResult
-from gitlabdata.orchestration_utils import query_executor
+from gitlabdata.orchestration_utils import query_dataframe
 from gitlabdata.orchestration_utils import data_science_engine_factory
-
-ENCODING = "utf-8"
-
-
-def dbt_model_dependencies_list(model_name_list: list, resource_type: str) -> list:
-    """Takes in a list of models, returns the model and its dependencies"""
-
-    # create empty lists to hold models
-    model_dependencies = []
-
-    # initialize dbt invoke
-    dbt = dbtRunner()
-
-    # for each create CLI args as a list of strings
-    for model in model_name_list:
-        cli_args = ["list", "--select", f"{model}+", f"--resource-type={resource_type}"]
-
-        # run the command for models
-        res_models: dbtRunnerResult = dbt.invoke(cli_args)
-
-        # get counts
-        if res_models.result is not None:
-            count_dependencies = len(res_models.result)
-        else:
-            count_dependencies = 0
-
-        # append results in table for models
-        model_dependencies.append(
-            {
-                "model_name": f"{model}",
-                "dependencies": res_models.result,
-                "count_dependencies": count_dependencies,
-            }
-        )
-
-    return model_dependencies
-
-
-def get_file_name() -> str:
-    """
-    Find directory name
-
-    Navigate to the file models_to_check.csv and add the dbt model names (ex. fct_behavior_structured_event).
-    The file should contain a list of model names to test with no header, with one model name per line.
-
-    example file list:
-    gitlab_dotcom_audit_events
-    dim_crm_opportunity
-    """
-    dirname = os.getcwd()
-    return f"{dirname}/models_to_check.csv"
-
-
-# Create empty set to append the cleaned model names to
-
-
-def get_model_names(file_name: str) -> set:
-    """
-    open file and add models to the list
-    """
-    models = set()
-    if os.path.exists(file_name):
-        with open(file=file_name, mode="r", newline="", encoding=ENCODING) as file:
-            lines = file.readlines()
-            for line in lines:
-                models.add(line.strip().lower())
-
-    else:
-        raise FileNotFoundError(
-            f"File {file_name} not found! Please create one and fill the data as per instruction"
-        )
-    return models
-
-
-def df_to_csv(file_name: str, df: pd.DataFrame) -> None:
-    """
-    create csv of the output called models_with_dependencies.csv
-    """
-    df.to_csv(file_name, index=False, encoding=ENCODING)
-
-
-def generate_results(model_names: list) -> None:
-    """
-    generate results
-    Note: model dependencies count includes the model
-    requested + all downstream, so the minimum number of dependencies is 1
-    """
-    models_with_dependencies = dbt_model_dependencies_list(
-        model_name_list=model_names, resource_type="model"
-    )
-    models_with_exposures = dbt_model_dependencies_list(
-        model_name_list=model_names, resource_type="exposure"
-    )
-
-    # convert lists to dataframes in order to merge the results into a single output
-    models_with_dependencies_df = pd.DataFrame(models_with_dependencies).rename(
-        columns={
-            "model_name": "model_name",
-            "dependencies": "dbt_model_dependencies",
-            "count_dependencies": "dbt_model_dependencies_count",
-        }
-    )
-    models_with_exposures_df = pd.DataFrame(models_with_exposures).rename(
-        columns={
-            "model_name": "model_name",
-            "dependencies": "ds_exposure_dependencies",
-            "count_dependencies": "ds_exposure_dependencies_count",
-        }
-    )
-
-    # merge the two results on model name to get a single output
-    models_with_dependencies_and_exposures = pd.merge(
-        models_with_dependencies_df, models_with_exposures_df, on="model_name"
-    )
-
-    df_to_csv(
-        file_name="models_with_dependencies.csv",
-        df=models_with_dependencies_and_exposures,
-    )
 
 
 def run():
@@ -141,24 +19,84 @@ def run():
     # model_names = get_model_names(file_name=file_name)
     # generate_results(model_names=model_names)
 
+    with open("./target/manifest.json", "r") as read_manifest:
+        manifest_data = json.load(read_manifest)
+
     with open("./target/run_results.json", "r") as read_file:
         result_data = json.load(read_file)
 
+    results = []
+
     for res in result_data.get("results"):
-        print(
-            f"{res.get('unique_id')} {result_data.get('metadata').get('generated_at')} {res.get('adapter_response').get('rows_affected')} {res.get('execution_time')}"
+
+        node_manifest = manifest_data.get("nodes").get(res.get("unique_id"))
+        node_config = node_manifest.get("config")
+        node_id = res.get("unique_id")
+        invocation_command = result_data.get("args").get("invocation_command")
+        invocation_is_full_refresh = (
+            True if invocation_command.find("--full-refresh") > 0 else False
         )
+        run_started_at = result_data.get("metadata").get("generated_at")
+        rows_affected = res.get("adapter_response").get("rows_affected")
+        execution_time = res.get("execution_time")
+        materialized = node_config.get("materialized")
+        full_refresh = (
+            invocation_is_full_refresh
+            if node_config.get("full_refresh") is None
+            else node_config.get("full_refresh")
+        )
+        node_data = {
+            "node_id": node_id,
+            "run_started_at": run_started_at,
+            "rows_affected": rows_affected,
+            "execution_time": execution_time,
+            "materialized": materialized,
+            "full_refresh": full_refresh,
+        }
+        results.append(node_data)
+
+    node_data_df = pd.DataFrame(results)
+    # print(node_data_df.dtypes)
+    # for node in manifest_data.get("nodes").get(res.get("unique_id")):
+    #    print(node.get("unique_id").value)
 
     # print(result_data)
 
-    # query_engine = data_science_engine_factory()
+    query_engine = data_science_engine_factory()
 
-    # query = """
-    # select * from pempey_prod.workspace_data.data_type_mock_table
-    # """
+    query = f"""
+      select
+          node_id,
+          run_started_at,
+          rows_affected,
+          total_node_runtime AS execution_time,              
+          materialization AS materialized,
+          was_full_refresh AS full_refresh
+        from prod.workspace_data.fct_dbt__model_executions
+        WHERE run_started_at > DATEADD('day',-35,CURRENT_DATE)
+        AND run_started_at < DATE_TRUNC('day',CURRENT_DATE)
+        AND status = 'success'
+        --AND node_id ='{node_id}'
+        --AND was_full_refresh = {full_refresh}
+        QUALIFY row_number() OVER (PARTITION BY node_id, was_full_refresh ORDER BY fct_dbt__model_executions.run_started_at DESC) = 1
+    """
 
-    # query_result = query_executor(query_engine, query)
-    # print(query_result)
+    query_result = query_dataframe(query_engine, query)
+    # print(query_result.dtypes)
+
+    node_result_df = node_data_df.set_index(["node_id", "full_refresh"]).join(
+        query_result.set_index(["node_id", "full_refresh"]),
+        lsuffix="_dev",
+        rsuffix="_prod",
+        how="inner",
+    )
+    print(node_result_df)
+
+    node_result_df.to_csv(
+        "__performance_check.csv",
+        index=True,
+        encoding="utf-8",
+    )
 
     # dbt = dbtRunner()
 
